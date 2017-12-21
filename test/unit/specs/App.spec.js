3d208c722007777bd1c340dc05bd51888276386f
jpietrzyk <jacpie3k@gmail.com>
2017-12-21 12:06:32 +0100

    Add basic unit tests for components

---
+++ test/unit/specs/App.spec.js
@@ -0,0 +1,22 @@
+import { shallow } from 'vue-test-utils'
+import App from '@/App'
+
+describe('App.test.js', () => {
+  let cmp
+
+  beforeEach(() => {
+    cmp = shallow(App, { // Create a shallow instance of the component
+      data: {
+        messages: ['Cat']
+      }
+    })
+  })
+
+  it('equals messages to ["Cat"]', () => {
+    expect(cmp.vm.messages).toEqual(['Cat'])
+  })
+
+  it('has the expected html structure', () => {
+    expect(cmp.element).toMatchSnapshot()
+  })
+})
